b48f6cc98fd5d190766503ff5f0a5267b06eff5d
Juliana Gomez <juliana@buffer.com>
2018-12-21 09:58:06 -0500

    personalizes npx card

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Juliana Gomez /'),
+  handle: chalk.cyan('gmzjuliana'),
+  work: chalk.white('Front End Engineer'),
+  twitter: chalk.cyan('https://twitter.com/gmzjuliana'),
+  github: chalk.cyan('https://github.com/gomezjuliana'),
+  linkedin: chalk.cyan('https://linkedin.com/in/gomezjuliana'),
+  web: chalk.cyan('https://gomezjuliana.com'),
+  npx: chalk.white('npx juliana'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
